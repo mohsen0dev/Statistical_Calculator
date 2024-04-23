5b1372d3605d45839b02e23d5d67e580f2020e7c
Mehrab Heidary <94475491+mehrab-heidary@users.noreply.github.com>
2024-04-23 07:17:15 -0700

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:simple_statistical_calculator/Core/constants/my_app.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:simple_statistical_calculator/Core/widgets/controller/custom_appbar_controller.dart';
 
 void main() {
-  runApp(MyApp());
+  _loadThemeMode();
+  runApp(const MyApp());
 }
-        
+
+final customAppBarClr = Get.put(CustomAppbarController());
+Future<void> _loadThemeMode() async {
+  final numberMode = await SharedPreferences.getInstance();
+  final storedMode = numberMode.getInt('mode');
+  customAppBarClr.selectedLDMode = storedMode ?? 0;
+}
